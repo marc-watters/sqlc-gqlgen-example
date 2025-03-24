5fc80f94c7527fa06c96589cdd5a9453dce3d36c
Marc Watters <marc.watters@proton.me>
2025-03-24 19:54:44 -0400

    feat(database) + table: authors

---
+++ database/init/02_authors.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS authors (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    website TEXT,
+    agent_id BIGINT NOT NULL,
+    FOREIGN KEY (agent_id) REFERENCES agents(id) ON DELETE RESTRICT
+);
