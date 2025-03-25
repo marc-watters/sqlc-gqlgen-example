44fae8e0ac506d1833948f119f0ad39eea81f6d7
Marc Watters <marc.watters@proton.me>
2025-03-25 12:50:32 -0400

    feat(gqlg) + resolver: inject Repository dependency
    We add the pgx.Repository interface as a field on the Resolver type.
    This is how the individual resolver methods will be able to access the
    data stored in our database.

--- api/gqlgen/resolver.go
+++ api/gqlgen/resolver.go
@@ -1,7 +1,11 @@
 package gqlgen
 
+import "github.com/marc-watters/sqlc-gqlgen-example/v2/pgx"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	Repository pgx.Repository
+}
