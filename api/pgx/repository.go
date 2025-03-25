c9da7b802304d0544a19385661f779fc9c3d192c
Marc Watters <marc.watters@proton.me>
2025-03-24 22:10:01 -0400

    feat(pgx) + interface: Repository
    While it would be possible to import and use the *Queries type directly
    in our GraphQL resolvers, taking this approach would make testing the
    resolver code difficult later on. The resolvers would end up coupled
    with the database code and we would most likely have no choice but to
    rely on integration tests in order to test them. We will opt for a
    different approach and create a custom Repository interface to serve as,
    an interface to the sqlc-generated code. The rest of our application
    will only communicate with the database through that interface, which
    means we will be able to use the mock version of the Repository in our
    tests.
    
    The Repository interface is a subset of query methods generated for us
    by sqlc along with a couple of custom methods. It only exposes a subset
    of the queries because some of the queries we have created were never
    meant to be used as stand-alone operations so the rest of our
    application neither has to nor should be able to access them directly.
    
    You'll notice that two methods of the Repository interface - CreateBook
    and UpdateBook - have signatures that differ from those of their
    namesakes from the queries.sql.go file. The generated CreateBook and
    UpdateBook methods are only concerned with making changes to the books
    table, which means they don't know anything about the authors of a given
    book. The complete process of creating or updating a book must include
    creating or updating the associations between the book and its authors
    and as such it will have to take the form of a sequence of database
    operations wrapped in a transaction. We will need to create custom
    implementations for these methods.

---
+++ api/pgx/repository.go
@@ -0,0 +1,32 @@
+package pgx
+
+import "context"
+
+// Repository is the application's data layer functionality.
+type Repository interface {
+	// agent queries
+	CreateAgent(ctx context.Context, arg CreateAgentParams) (*Agent, error)
+	DeleteAgent(ctx context.Context, id int64) (*Agent, error)
+	GetAgent(ctx context.Context, id int64) (*Agent, error)
+	ListAgents(ctx context.Context) ([]*Agent, error)
+	ListAgentsByAuthorIDs(ctx context.Context, authorIDs []int64) ([]*ListAgentsByAuthorIDsRow, error)
+	UpdateAgent(ctx context.Context, arg UpdateAgentParams) (*Agent, error)
+	// author queries
+	CreateAuthor(ctx context.Context, arg CreateAuthorParams) (*Author, error)
+	DeleteAuthor(ctx context.Context, id int64) (*Author, error)
+	GetAuthor(ctx context.Context, id int64) (*Author, error)
+	ListAuthors(ctx context.Context) ([]*Author, error)
+	ListAuthorsByAgentID(ctx context.Context, agentID int64) ([]*Author, error)
+	ListAuthorsByAgentIDs(ctx context.Context, agentIDs []int64) ([]*Author, error)
+	ListAuthorsByBookID(ctx context.Context, bookID int64) ([]*Author, error)
+	ListAuthorsByBookIDs(ctx context.Context, bookIDs []int64) ([]*ListAuthorsByBookIDsRow, error)
+	UpdateAuthor(ctx context.Context, arg UpdateAuthorParams) (*Author, error)
+	// book queries
+	CreateBook(ctx context.Context, bookArg CreateBookParams, authorIDs []int64) (*Book, error)
+	UpdateBook(ctx context.Context, bookArg UpdateBookParams, authorIDs []int64) (*Book, error)
+	DeleteBook(ctx context.Context, id int64) (*Book, error)
+	GetBook(ctx context.Context, id int64) (*Book, error)
+	ListBooks(ctx context.Context) ([]*Book, error)
+	ListBooksByAuthorID(ctx context.Context, authorID int64) ([]*Book, error)
+	ListBooksByAuthorIDs(ctx context.Context, authorIDs []int64) ([]*ListAuthorsByBookIDsRow, error)
+}
