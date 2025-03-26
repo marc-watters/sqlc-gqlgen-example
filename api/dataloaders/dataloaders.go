8fd3b9d4727f71e1f628ec2cd78e5f7c148991ab
Marc Watters <marc.watters@proton.me>
2025-03-26 16:19:40 -0400

    feat(dataloaders) + type: loaders with constructor
    The exported Loaders type is an empty struct to begin with, but it will
    soon hold the various dataloader implementations used by our
    application.
    
    The newLoaders function will be used to instantiate the dataloaders. The
    function takes the context of the current request (ctx) and the
    repository (pgx.Repository) as arguments, the latter because the
    dataloaders will need to query the database.

--- api/dataloaders/dataloaders.go
+++ api/dataloaders/dataloaders.go
@@ -1,5 +1,19 @@
 package dataloaders
 
-type contextKey string
+import (
+	"context"
+
+	"github.com/marc-watters/sqlc-gqlgen-example/v2/pgx"
+)
+
+type (
+	contextKey string
+
+	Loaders struct{}
+)
 
 const key = contextKey("dataloaders")
+
+func newLoaders(ctx context.Context, repoSvc pgx.Repository) *Loaders {
+	return &Loaders{}
+}
