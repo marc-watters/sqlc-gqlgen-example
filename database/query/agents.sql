a56a25eb74d5b5c57162d14505f27b9fe01dd572
Marc Watters <marc.watters@proton.me>
2025-03-26 14:14:23 -0400

    feat(database) + query: list agents by author IDs

--- database/query/agents.sql
+++ database/query/agents.sql
@@ -6,6 +6,10 @@ WHERE id = $1;
 SELECT * FROM agents
 ORDER BY name;
 
+-- name: ListAgentsByAuthorIDs :many
+SELECT agents.*, authors.id AS author_id FROM agents, authors
+WHERE agents.id = authors.agent_id AND authors.id  = ANY($1::bigint[]);
+
 -- name: CreateAgent :one
 INSERT INTO agents (name, email)
 VALUES ($1, $2)
