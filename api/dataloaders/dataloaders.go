9e3cf8b36826d51a9ff1bbeeb2e600aaf48e86bb
Marc Watters <marc.watters@proton.me>
2025-03-27 11:00:13 -0400

    feat(dataloaders) + loaders: AgentLoader

--- api/dataloaders/dataloaders.go
+++ api/dataloaders/dataloaders.go
@@ -6,6 +6,7 @@ package dataloaders
 
 import (
 	"context"
+	"time"
 
 	"github.com/marc-watters/sqlc-gqlgen-example/v2/pgx"
 )
@@ -13,11 +14,44 @@ import (
 type (
 	contextKey string
 
-	Loaders struct{}
+	Loaders struct {
+		AgentByAuthorID *AgentLoader
+	}
 )
 
 const key = contextKey("dataloaders")
 
 func newLoaders(ctx context.Context, repoSvc pgx.Repository) *Loaders {
-	return &Loaders{}
+	return &Loaders{
+		AgentByAuthorID: newAgentByAuthorID(ctx, repoSvc),
+	}
+}
+
+func newAgentByAuthorID(ctx context.Context, repo pgx.Repository) *AgentLoader {
+	return NewAgentLoader(AgentLoaderConfig{
+		MaxBatch: 100,
+		Wait:     5 * time.Millisecond,
+		Fetch: func(authorIDs []int64) ([]*pgx.Agent, []error) {
+			// db query
+			res, err := repo.ListAgentsByAuthorIDs(ctx, authorIDs)
+			if err != nil {
+				return nil, []error{err}
+			}
+			// map
+			groupByAuthorID := make(map[int64]*pgx.Agent, len(authorIDs))
+			for _, r := range res {
+				groupByAuthorID[r.AuthorID] = &pgx.Agent{
+					ID:    r.ID,
+					Name:  r.Name,
+					Email: r.Email,
+				}
+			}
+			// order
+			result := make([]*pgx.Agent, len(authorIDs))
+			for i, authorID := range authorIDs {
+				result[i] = groupByAuthorID[authorID]
+			}
+			return result, nil
+		},
+	})
 }
