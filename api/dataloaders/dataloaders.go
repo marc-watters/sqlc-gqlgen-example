dd3c603d1ccf83cd7ed6d23a3ffbde5b8cfb8fed
Marc Watters <marc.watters@proton.me>
2025-03-27 11:24:09 -0400

    build(dataloaden) + generate: AuthorSliceLoader
    The input type of the new dataloader is int64 since this is the type of
    the agent id values. The output is a slice of author object pointers, or
    []*pgx.Author. The fact that the new dataloader returns a slice is, by
    convention, reflected in the name of the new dataloader type.

--- api/dataloaders/dataloaders.go
+++ api/dataloaders/dataloaders.go
@@ -1,6 +1,7 @@
 package dataloaders
 
 //go:generate dataloaden AgentLoader int64 *github.com/marc-watters/sqlc-gqlgen-example/v2/pgx.Agent
+//go:generate dataloaden AuthorSliceLoader int64 []*github.com/marc-watters/sqlc-gqlgen-example/v2/pgx.Author
 
 import (
 	"context"
